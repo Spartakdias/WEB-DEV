c0be6600d271c64cb96e20c8f3eb24da4bbaf4d3
Spartakdias <d.kadyraly@gmail.com>
2025-02-07 22:54:12 +0500

    lab3

--- 3lab/2task/scrypt.js
+++ 3lab/2task/scrypt.js
@@ -3,6 +3,8 @@ const clickbutton = document.getElementById('button');
 const list = document.getElementById('list');
 const button = document.getElementById('btn');
 
+document.addEventListener('DOMContentLoaded', loadTasks);
+
 clickbutton.addEventListener('click', () => {
     addTask();
 });
@@ -31,14 +33,17 @@ function addTask() {
         input.value = '';
 
         updateDelAllButton();
+        saveTasks();
 
         delOneButton.addEventListener('click', () => {
             li.remove();
+            saveTasks();
             updateDelAllButton();
         });
 
         doneButton.addEventListener('click', () => {
             li.style.textDecoration = 'line-through';
+            saveTasks();
         });
     }
 }
@@ -55,8 +60,28 @@ function updateDelAllButton() {
         newDelAllButton.addEventListener('click', () => {
             list.innerHTML = '';
             newDelAllButton.remove();
+            saveTasks();
         });
     } else if (list.childNodes.length === 0 && delAllButton ) {
         delAllButton.remove();
     }
+}
+
+function saveTasks() {
+    const tasks = [];   
+    list.querySelectorAll('li').forEach(li => {
+        tasks.push({
+            text: li.childNodes[0].nodeValue.trim()
+        });
+    });
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+
+function loadTasks() {
+    const savedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    savedTasks.forEach(task => {
+        input.value = task.text;
+        addTask();
+    });
 }
