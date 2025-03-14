31ce3bd33841c0f07a363488ff3b45f09ddc1b0a
Spartakdias <d.kadyraly@gmail.com>
2025-03-14 11:51:28 +0500

    modify

--- lab6/src/app/albums/albums.service.ts
+++ lab6/src/app/albums/albums.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -7,12 +8,20 @@ import { HttpClient } from '@angular/common/http';
 export class AlbumsService {
   private apiUrl = 'https://jsonplaceholder.typicode.com/albums';
 
-
   constructor(private http: HttpClient) {}
 
-  getAlbums() {
+
+  getAlbums(): Observable<any[]> {
     return this.http.get<any[]>(this.apiUrl);
   }
 
 
+  getAlbumById(id: number): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/${id}`);
+  }
+
+
+  deleteAlbum(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
 }
